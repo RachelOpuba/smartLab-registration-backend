6375b4117732b7b9ac0108b7f272f65cdefae2b3
Rachel Opuba <ogbenirach@gmail.com>
2024-05-30 12:54:34 +0100

    Fixed database whitelis ip address issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT =  5000
 const Student = require("./models/studentSchema");
 
 app.use(express.json());
@@ -32,45 +32,45 @@ app.get("/api/students", async (req, res) => {
   }
 });
 
-// app.get("/api/students/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const student = await Student.findById(id);
-//     res.status(200).json(student);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+app.get("/api/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findById(id);
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-// app.put("/api/students/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const student = await Student.findByIdAndUpdate(id, req.body);
+app.put("/api/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findByIdAndUpdate(id, req.body);
 
-//     if (!student) {
-//       return res.status(404).json({ message: "Student not found" });
-//     }
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
 
-//     const updatedStudent = await Student.findById(id);
-//     res.status(200).json(updatedStudent);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+    const updatedStudent = await Student.findById(id);
+    res.status(200).json(updatedStudent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-// app.delete("/api/students/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const student = await Student.findByIdAndDelete(id);
+app.delete("/api/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findByIdAndDelete(id);
 
-//     if (!student) {
-//       return res.status(404).json({ message: "Student not found" });
-//     }
-//     res.status(200).json({ message: "Student deleted Successfylly" });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+    res.status(200).json({ message: "Student deleted Successfylly" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 mongoose
   .connect(
     "mongodb+srv://smartlab:BZxePeg6N0osDufw@cluster0.unepvpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -81,6 +81,7 @@ mongoose
       console.log(`Server Started at ${PORT}`);
     });
   })
-  .catch(() => {
+  .catch((err) => {
+    console.log(err)
     console.log("Connection falled");
   });
